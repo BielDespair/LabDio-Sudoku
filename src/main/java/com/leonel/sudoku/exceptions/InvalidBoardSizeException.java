62ca5a8649afae0edfab6b37fe772288ac90c82c
Gabriel Leonel <gabrielvista7@gmail.com>
2025-04-30 17:16:52 -0300

    Deleted sudoku package

--- src/main/java/com/leonel/sudoku/exceptions/InvalidBoardSizeException.java
+++
@@ -1,7 +0,0 @@
-package com.leonel.sudoku.exceptions;
-
-public class InvalidBoardSizeException extends RuntimeException {
-    public InvalidBoardSizeException() {
-      super("Invalid board size: must be greater than 1 and a perfect square (e.g., 4, 9, 16).");
-    }
-}
